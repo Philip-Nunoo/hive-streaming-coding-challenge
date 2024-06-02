10682e50f1c116aebf70dcbb2d5b5867f88e5136
Philip Nunoo <phanunoo@gmail.com>
2024-06-03 01:22:43 +0200

    chore: Update express package to version 4.19.2 and add express-list-endpoints package

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cpuUsageRoutes from './routes/cpuUsageRoutes';
+import listEndpoints from 'express-list-endpoints';
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use('/api/cpu', cpuUsageRoutes);
+
+const endpoints = listEndpoints(app);
+endpoints.forEach((endpoint) => {
+  endpoint.methods.forEach((method) => {
+    console.log(`Endpoint: ${method} ${endpoint.path}`);
+  });
+});
 
 export default app;
